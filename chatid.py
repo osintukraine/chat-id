f01e211824072c316a3f0387fcdb6b3e0d893852
BenB <info@buzzworkers.com>
2023-07-30 19:59:22 +0200

    add admin info

--- chatid.py
+++ chatid.py
@@ -2,6 +2,10 @@ import os
 import json
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.errors.rpcerrorlist import ChatAdminRequiredError
 
 # Load the API ID and hash from the .env file
 load_dotenv()
@@ -24,13 +28,36 @@ channel = client.get_entity(chat_id)
 channel_name = channel.title
 channel_link = channel.username
 
-# Create a dictionary with the channel name and link
+# Get the full channel information
+full = client(GetFullChannelRequest(channel))
+about = full.full_chat.about
+
+# Try to get the administrators of the chat
+try:
+    admins = client(GetParticipantsRequest(
+        channel, ChannelParticipantsAdmins(), 0, 100, hash=0
+    ))
+
+    admin_info = []
+    for user in admins.users:
+        admin_info.append({
+            "Admin ID": user.id,
+            "Admin Username": user.username,
+            "Admin First Name": user.first_name,
+            "Admin Last Name": user.last_name
+        })
+except ChatAdminRequiredError:
+    admin_info = "Cannot retrieve admin info due to lack of permissions."
+
+# Create a dictionary with the channel name, link, about and admin info
 channel_info = {
     "Channel name": channel_name,
-    "Channel link": f"https://t.me/{channel_link}"
+    "Channel link": f"https://t.me/{channel_link}",
+    "About": about,
+    "Admin Info": admin_info
 }
 
 # Print the dictionary as a JSON object
-print(json.dumps(channel_info, indent=4))
+print(json.dumps(channel_info, indent=4, ensure_ascii=False))
 
 client.disconnect()
